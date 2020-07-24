f770afc89c4f9618421479e2068c89f4d4c6cde6
Jaka Kordež <jaka.kordez@gmail.com>
2020-07-24 15:41:20 +0200

    LAZReader reading multiple files

--- LAZReader/Program.cs
+++ LAZReader/Program.cs
@@ -11,25 +11,34 @@ namespace LAZReader
     {
         static void Main(string[] args)
         {
-            var lazReader = new laszip_dll();
-            var compressed = true;
-            var filePath = args.Length > 0 ? args[0] : "SloveniaLidarRGB0.laz"; //@"C:\Users\jakak\Desktop\nrg-sem-Potree\pointclouds\Normals\data\r\00000\r00000.laz";
-
-            lazReader.laszip_open_reader(filePath, ref compressed);
-            var numberOfPoints = lazReader.header.number_of_point_records;
-            var coordArray = new double[3];
-            Console.WriteLine("[{0:hh:mm:ss}] Reading LAZ... {1} points", DateTime.Now, numberOfPoints);
-            for (var pointIndex = 0; pointIndex < numberOfPoints; pointIndex++)
+            long points = 0;
+            foreach (var file in new System.IO.DirectoryInfo(Environment.CurrentDirectory).GetFiles())
             {
-                lazReader.laszip_read_point();
-                lazReader.laszip_get_coordinates(coordArray);
-                var point = lazReader.laszip_get_point_pointer();
-                var colors = point.rgb;
-                Console.WriteLine(String.Format("R {0:X} G {1:X} B {2:X}", colors[0], colors[1], colors[2]));
+                var lazReader = new laszip_dll();
+                var compressed = true;
+                var filePath = args.Length > 0 ? args[0] : "SloveniaLidarRGB0.laz"; //@"C:\Users\jakak\Desktop\nrg-sem-Potree\pointclouds\Normals\data\r\00000\r00000.laz";
+                filePath = file.FullName;
 
-                if (pointIndex % 20 == 0 && Console.ReadKey().Key == ConsoleKey.Q) break;
+                lazReader.laszip_open_reader(filePath, ref compressed);
+                var numberOfPoints = lazReader.header.number_of_point_records;
+                var coordArray = new double[3];
+                points += numberOfPoints;
+                Console.WriteLine("[{0:hh:mm:ss}] Reading LAZ... {1} points starting at {2}", DateTime.Now, numberOfPoints, lazReader.header.offset_to_point_data);
+                
+                for (var pointIndex = 0; pointIndex < numberOfPoints; pointIndex++)
+                {
+                    lazReader.laszip_read_point();
+                    lazReader.laszip_get_coordinates(coordArray);
+                    var point = lazReader.laszip_get_point_pointer();
+                    var colors = point.rgb;
+                    Console.WriteLine(String.Format("X {0} Y {1} Z {2} R {3:X} G {4:X} B {5:X}", point.X, point.Y, point.Z, colors[0], colors[1], colors[2]));
+
+                    if (pointIndex % 20 == 0 && Console.ReadKey().Key == ConsoleKey.Q) break;
+                }
+                Console.WriteLine("[DONE] ");
             }
-            Console.WriteLine("[DONE] ");
+            Console.WriteLine("Total: " + points);
+            Console.Read();
         }
     }
 }
